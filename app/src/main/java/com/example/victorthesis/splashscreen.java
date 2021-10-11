5873c20b9d66535f8a8814c47e041e93c62bf45d
PapaWashfolife <natividadleandro.ln@gmail.com>
2021-10-12 06:04:07 +0800

    VictorThesis

---
+++ app/src/main/java/com/example/victorthesis/splashscreen.java
@@ -0,0 +1,18 @@
+package com.example.victorthesis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent( this, Login.class));
+
+    }
+
+
+}
